64b828f28b4e5f25024d51806836900124b1e69c
Franco Viotti <fviotti@mi.unc.edu.ar>
2022-10-15 13:15:19 -0300

    Se corrigen los registros de PINSEL y PINMODE, ademas de la seleccion de los canales

--- EjerciciosFranco/config_ADC_2.c
+++ EjerciciosFranco/config_ADC_2.c
@@ -28,16 +28,16 @@ void config_ADC(){
 
 	LPC_SC->PCLKSEL0|=	(3<<24); 	//Seleccionamos division del CCLK/8 para el ADC
 	LPC_ADC->ADCR 	&=	(0x00<<8); 	// Esta es la frecuencia final de trabajo que le llega al ADC
-									// Ffinal = CCLK/PCLKSEL0/(AD0CR+1)
+								// Ffinal = CCLK/PCLKSEL0/(AD0CR+1)
 									// En nuestro caso, Ffinal = 100Mhz/8/(0+1)
 
 
-	LPC_PINCON->PINSEL1  	=	(0xAA<<14); //Los pines P0.23 P0.24 P0.25 P0.26 seran entradas al ADC
-	LPC_PINCON->PINMODE1 	= 	(0xAA<<14); //Resistencias pull-up pull-down, ambas deshabilitadas
+	LPC_PINCON->PINSEL1  	|=	(1<<14); //Los pines P0.23 P0.24 P0.25 P0.26 seran entradas al ADC
+	LPC_PINCON->PINMODE1 	|= 	(1<<15); //Resistencias pull-up pull-down, ambas deshabilitadas
 
 	LPC_ADC->ADCR	|=	(1<<16); //Habilitamos el modo burst
 
-	LPC_ADC->ADCR	|=	(0x15); //Habilitamos AD0, AD1, AD2 y AD3
+	LPC_ADC->ADCR	|=	(0xF); //Habilitamos AD0, AD1, AD2 y AD3
 
 	return;
 }
